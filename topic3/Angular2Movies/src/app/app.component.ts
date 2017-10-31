164b02f769f878bf76747f46427d1a1f5894cdf8
victorja <abeledovictor@gmail.com>
2017-10-31 19:31:43 -0300

    added movies and edit option

--- topic3/Angular2Movies/src/app/app.component.ts
+++ topic3/Angular2Movies/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+export class Movie {
+  id: number;
+  name: string;
+}
+
+const MOVIES: Movie[] = [
+  { id: 11, name: 'Spongebob' },
+  { id: 12, name: 'MIB' },
+  { id: 13, name: 'Scooby Doo' },
+  { id: 14, name: 'Superbad' },
+  { id: 15, name: 'Spider-man' },
+  { id: 16, name: 'Ratatouille' },
+  { id: 17, name: 'Aladdin' },
+  { id: 18, name: 'Lion King' },
+  { id: 19, name: 'Cinderella' },
+  { id: 20, name: 'Pinoccio' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Movie list page';
+  movies: Movie[] = MOVIES;
+  selectedMovie: Movie;
+
+  onSelect(movie: Movie): void {
+    this.selectedMovie = movie;
+  }
 }
