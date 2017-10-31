6d7063188970d03baa29888b92671f3423b05b42
victorja <abeledovictor@gmail.com>
2017-10-31 18:31:01 -0300

    newproject installed

---
+++ topic3/quickstart/movie-app/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieDetailComponent } from '../movie-detail/movie-detail.component';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ topic3/quickstart/movie-app/src/app/movie.ts
@@ -0,0 +1,4 @@
+export class Movie {
+    movieName: number;//ex id
+    info: string;//ex name
+  }
