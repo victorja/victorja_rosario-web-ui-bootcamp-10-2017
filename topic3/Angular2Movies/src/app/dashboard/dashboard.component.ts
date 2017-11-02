ddb450deb7c595a1b0d77fd9648bf111200ac4f1
Victor Abeledo <victor.jabeledo@gmail.com>
2017-11-02 14:36:32 -0300

    update movie import

--- topic3/Angular2Movies/src/app/dashboard/dashboard.component.ts
+++ topic3/Angular2Movies/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Movie } from '../models/movie';
+import { Movie } from '../models/movie.model';
 import { MovieService } from '../services/movie.service';
 
 @Component({
